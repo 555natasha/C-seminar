049ff951035cc0a79aac8e5664cb664da30a3b85
NatASha <nash_ka@mail.ru>
2023-12-13 15:50:41 +0300

    005

---
+++ 005/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число 
+// и на выходе показывает последнее число этого числа
+System.Console.WriteLine("Введите трехзначное число: ");
+string str = Console.ReadLine(); // считываем вводимую строку
+int NNN = Convert.ToInt32(str); // строку конвертируем в число
+if (NNN > 99 && NNN < 1000)
+{
+    int nnn=NNN%10;
+    Console.WriteLine(nnn);
+}
+else
+{
+    Console.WriteLine("вы ввели не трехзначное число");
+}
