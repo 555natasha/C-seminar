d047aa78e178617956a339e88c4d2c86b7ac21cb
NatASha <nash_ka@mail.ru>
2023-12-13 17:20:04 +0300

    008-DZ3

---
+++ 008-DZ3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Написать программу, которая принимает на вход число и выдаёт, чётное оно или нечётное.
+System.Console.WriteLine("Введите число: ");
+string str = Console.ReadLine(); // считываем вводимую строку
+int num = Convert.ToInt32(str); // строку конвертируем в число
+
+if(num%2 == 0) {Console.WriteLine("Число чётное");}
+else {Console.WriteLine("Число нечётное");}
