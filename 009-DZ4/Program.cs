6bb633c867bbdfc7ef09c8db46ea1cc713de38f9
NatASha <nash_ka@mail.ru>
2023-12-17 13:33:36 +0300

    009-DZ4

---
+++ 009-DZ4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу, которая принимает на вход число N, 
+// а на выходе выдает все чётные числа от 1 до N.
+System.Console.WriteLine("Введите число: ");
+string str = Console.ReadLine(); // считываем вводимую строку
+int N = Convert.ToInt32(str); // строку конвертируем в число
+int count = 1;
+int ch = 0;
+while(count < N) 
+{
+    ch = count;
+    if(ch % 2 == 0)
+    {
+        Console.Write($" {ch}");
+    }
+    count++;
+}
