065a157b3b02fb6f116bef94d877e4a09ac46a97
NatASha <nash_ka@mail.ru>
2023-12-20 22:18:31 +0300

    017-sem2-mass

---
+++ 017-sem2-mass/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и удаляет вторую цифру этого числа.
+
+
+int number = new Random().Next(100,1000);
+Console.WriteLine($"Число {number}");
+int num1 = number / 100;
+int num2 = number % 10;
+// int result = num1*10 + num2;
+// Console.WriteLine(result);
+
+Console.WriteLine($"Получаемое число {num1}{num2}");
