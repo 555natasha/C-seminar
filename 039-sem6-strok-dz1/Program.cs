013612232423d34e0f7acf4e0f934a794b3f64bf
NatASha <nash_ka@mail.ru>
2023-12-31 03:02:57 +0300

    039-sem6-strok-dz1

---
+++ 039-sem6-strok-dz1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте двумерный массив символов (тип char[,]). Создать строку из символов этого массива.
+// a b c => “abcdef”
+// d e f
+
+using System.Text;
+
+char[,] arr = {{'a','b','c'},{'d','e','f'}};
+Console.WriteLine("Наш массив:");
+PrintCharArray(arr);
+
+Console.Write("\nСтрока: ");
+string strok = CreatStrok(arr);
+Console.WriteLine(strok);
+
+void PrintCharArray(char[,] list)
+{
+    for (int i = 0; i < list.GetLength(0); i++)    
+    {
+        for (int j = 0; j < list.GetLength(1); j++)
+        {
+            Console.Write(list[i, j] + " "); // Интерполяция строк
+        }
+        Console.WriteLine();
+    }
+}
+
+string CreatStrok(char[,] mass)
+{
+    StringBuilder strB = new StringBuilder();
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            strB.Append(mass[i,j]);
+           // st += mass[i,j];
+        }
+    }
+    string str = strB.ToString();
+    return str;
+}
