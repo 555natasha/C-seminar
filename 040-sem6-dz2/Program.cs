72d65a247d7a62b34614973107e9d1594e966213
NatASha <nash_ka@mail.ru>
2023-12-31 03:04:13 +0300

    040-sem6-dz2

---
+++ 040-sem6-dz2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные. 
+// “aBcD1ef!-” => “abcd1ef!-”
+
+string str = GetString();
+string result = str.ToLower();
+Console.WriteLine($"Понижен регистр: {result}");
+
+/*------------Методы-------------*/
+string GetString()
+{
+    Console.Write("Введите строку: ");
+    string str = Console.ReadLine();
+    return str;
+}
+
+bool IsVowel(char c)
+{
+    string vovels = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!-., ?:;";
+    return vovels.Contains(c); // если этот элемент есть, то он возвращает тру
+}
